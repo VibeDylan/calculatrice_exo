76afd933fc73282e958903dcd9714cf9cf37262f
Flora Trecul <flora.trecul@gmail.com>
2025-12-02 11:07:54 +0100

    fix: handled input and errors for division

--- src/operations/division.py
+++ src/operations/division.py
@@ -1,5 +1,15 @@
 def divide_numbers(a, b):
+	a = input("Entrez un premier nombre :")
+	b = input("Entrez un autre nombre :")
+
 	if b == 0:
-		print("Un nombre ne peut être divisé par zéro.")
+		print("Un nombre ne peut être divisé par zéro")
 	else:
-		return a / b
+		try:
+			a = float(a)
+			b = float(b)
+		except:
+			print("Veuillez entrer des nombres")
+
+		result = a / b
+		print(f"{a} / {b} = {result}")
