a4b1114f36e52f5f41ca962b87961f7403c6800a
Flora Trecul <flora.trecul@gmail.com>
2025-12-02 10:56:22 +0100

    fix: handle input and error for multiplication

--- src/operations/multiplication.py
+++ src/operations/multiplication.py
@@ -1,2 +1,12 @@
 def multiply_numbers(a, b):
-	return a * b
+	a = input("Entrez un premier nombre :")
+	b = input("Entrez un autre nombre :")
+
+	try:
+		a = float(a)
+		b = float(b)
+	except:
+		print("Veuillez entrer des nombres")
+
+	result = a * b
+	print(f"{a} x {b} = {result}")
